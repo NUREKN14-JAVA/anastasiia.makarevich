d0d29fb59c71599cd2a1f5e97ddb96ffb5d677da
Ana Makarevich <electric.blake@gmail.com>
2016-09-28 12:09:22 +0300

    Improve the code style (add comments, add spaces, replace tabs by spaces)

--- usermanagement/src/main/java/com/anamakarevich/usermanagement/User.java
+++ usermanagement/src/main/java/com/anamakarevich/usermanagement/User.java
@@ -3,45 +3,75 @@ package com.anamakarevich.usermanagement;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * @author ana_makarevich
+ * User class that describes the user
+ */
 public class User {
-	private Long id;
-	private String firstName;
-	private String lastName;
-	private Date dateOfBirthd;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public Date getDateOfBirthd() {
-		return dateOfBirthd;
-	}
-	public void setDateOfBirthd(Date dateOfBirthd) {
-		this.dateOfBirthd = dateOfBirthd;
-	}
-	public Object getFullName() {
-		return getLastName() + ", " + getFirstName();
-	}
-	public int getAge() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(new Date());
-		int currentYear = calendar.get(Calendar.YEAR);
-		calendar.setTime(getDateOfBirthd());
-		int year = calendar.get(Calendar.YEAR);
-		return currentYear - year;
-	}
-	
+
+    private Long id;
+    
+    private String firstName;
+    private String lastName;
+    
+    private Date dateOfBirthd;
+    
+    public Long getId() {
+        return id;
+        }
+    
+    public void setId(Long id) {
+        this.id = id;
+        }
+    
+    public String getFirstName() {
+        return firstName;
+        }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        }
+    
+    public String getLastName() {
+        return lastName;
+        }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        }
+    
+    public Date getDateOfBirthd() {
+        return dateOfBirthd;
+        }
+    
+    public void setDateOfBirthd(Date dateOfBirthd) {
+        this.dateOfBirthd = dateOfBirthd;
+        }
+    
+    /**
+     * 
+     * @return concatenation of last name and first name
+     */
+    public Object getFullName() {
+        return getLastName() + ", " + getFirstName();
+        }
+    
+    /**
+     * 
+     * @return difference between the current year and the day of birth of the user
+     */
+    public int getAge() {
+        
+        Calendar calendar = Calendar.getInstance();
+        
+        calendar.setTime(new Date());
+        int currentYear = calendar.get(Calendar.YEAR);
+        
+        calendar.setTime(getDateOfBirthd());
+        int year = calendar.get(Calendar.YEAR);
+        
+        return currentYear - year;
+        
+        }
+
 }
--- usermanagement/src/test/java/com/anamakarevich/usermanagement/UserTest.java
+++ usermanagement/src/test/java/com/anamakarevich/usermanagement/UserTest.java
@@ -9,29 +9,49 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class UserTest extends TestCase{
-	private User user;
-	private Date dateOfBirthd;
+    
+    private User user;
+    
+    private Date dateOfBirthd;
 
-	@Before
-	protected void setUp() throws Exception {
-		super.setUp();
-		user = new User();
-		
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(1941, Calendar.MAY,24);
-		dateOfBirthd = calendar.getTime();
-	}
-	
-	@Test 
-	public void testGetFullName() {
-		user.setFirstName("Bob");
-		user.setLastName("Dylan");
-		assertEquals("Dylan, Bob",  user.getFullName());
-	}
+    @Before
+    protected void setUp() throws Exception {
+        
+        super.setUp();
+        
+        user = new User();
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1941, Calendar.MAY,24);
+        
+        dateOfBirthd = calendar.getTime();
+        
+        }
 
-	@Test 
-	public void testGetAge() {
-		user.setDateOfBirthd(dateOfBirthd);
-		assertEquals(2016-1941, user.getAge());
-	}
+    @Test 
+    public void testGetFullName() {
+        
+        user.setFirstName("Bob");
+        user.setLastName("Dylan");
+        
+        assertEquals("Dylan, Bob",  user.getFullName());
+        
+        }
+
+    @Test 
+    public void testGetAge() {
+
+        user.setDateOfBirthd(dateOfBirthd);
+        
+        Calendar c2 = Calendar.getInstance();
+        c2.setTime(new Date());		
+        
+        Calendar c3 = Calendar.getInstance();
+        c3.setTime(user.getDateOfBirthd());
+        
+        assertEquals(c2.get(Calendar.YEAR) - c3.get(Calendar.YEAR), user.getAge());
+        
+        }
+
+
 }
